2eccd6e556c1779cab239aa63a8d2a7784c8e079
minh-bao <mbnguyen@hotmail.fr>
2023-06-04 18:53:17 +0200

    feat(ServiceProvider): add a mehods to the File facade to accept certain type of files

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Inertia\Inertia;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        File::macro('document', fn () => File::types(['pdf', 'rtf', 'doc', 'docx', 'txt', 'csv','jpg','jpeg', 'png', 'gif' ]));
+    }
+}
