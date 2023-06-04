43cfc2b926e21e2c11824375b31c2a2a82b4d35a
minh-bao <mbnguyen@hotmail.fr>
2023-06-04 22:56:12 +0200

    fix(ServiceProvider): remove unused namespace import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Inertia\Inertia;
 use Illuminate\Validation\Rules\File;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
