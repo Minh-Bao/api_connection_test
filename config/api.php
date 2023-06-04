e8b35f57b15192afc90aab3bcad989786e24c254
minh-bao <mbnguyen@hotmail.fr>
2023-06-04 18:42:09 +0200

    feat(config): add a config file for Ar24 api sensitive key

---
+++ config/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "ar24"  =>  [
+        'base_uri'      => env('AR24_API_URI'),
+        'private_key'   => env('AR24_PRIVATE_KEY'),
+        'secret'        => env('AR24_SECRET')
+    ]
+];
